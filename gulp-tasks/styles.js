9023e64f22690e061dc2a7562ad04563f69e11ae
Timur Svoboda <svobodatimur@gmail.com>
2022-01-31 23:02:32 +0200

    Write sourcemaps generation for styles task

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -2,9 +2,12 @@ import gulp from "gulp";
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 const sass = gulpSass(dartSass);
+import sourcemaps from "gulp-sourcemaps";
 
 export default function styles() {
   return gulp.src("src/scss/*.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("src/css"));
+    .pipe(sourcemaps.write("./"))
+    .pipe("src/css");
 }
