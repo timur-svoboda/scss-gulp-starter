c89533885b3f5b4b5d9a4417966a419b9ccfa4fe
Timur Svoboda <svobodatimur@gmail.com>
2022-02-09 23:08:08 +0200

    Write block: background-image

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+  // background-image
+  Array.from(document.querySelectorAll(".background-image")).forEach(bgi => {
+    if (bgi.dataset.src) {
+      bgi.style.backgroundImage = `url(${bgi.dataset.src})`;
+      delete bgi.dataset.src;
+    }
+  });
 });
